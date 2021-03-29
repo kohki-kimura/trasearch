0d5684a797501ce5d5292cbcba120a9a6ba80357
kohki-kimura <kohki.soccer0217@gmail.com>
2021-03-29 23:56:40 +0900

    TOPページ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,23 @@
-a
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>TraSearch</title>
+    <link href="{{asset('/assets/css/index.css')}}" rel="stylesheet">
+</head>
+
+<div class="top">
+  <img src="/assets/images/group266.png" >
+      <div class="copy">理想の体づくりには、理想のパートナーを</div>
+</div>
+
+<div>
+    <div class="description-title">TraSeatchとは...?<br></div>
+    <div class="description">
+        TraSearch(トレサーチ)は、自身の悩みに対して<br>
+        検索いらずでトレーニングの予約ができるWEBサービスです。<br>
+        自身の悩み、改善したい内容をSNS式で投稿するだけで<br>
+        その道のプロがあなたにあったプランを提案します。
+    </div>
+</div>
